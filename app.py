89e5b4a3063ebb525f34c86257c280d7bf25f0a9
samart1231 <samart1231@users.noreply.github.com>
2025-07-17 22:35:28 +0700

    test11

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import sqlite3
 from datetime import datetime, time
 import pandas as pd
 import io
+import os
 
 app = Flask(__name__)
 DB_FILE = 'ot.db'
@@ -590,6 +591,12 @@ def delete_holiday(id):
     return redirect('/holidays')
 
 
+# if __name__ == '__main__':
+#     init_db()
+#     app.run(host='0.0.0.0', port=5000)
+
+
 if __name__ == '__main__':
     init_db()
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
