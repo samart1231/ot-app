7fd45a57665163b45a8b35c24bfe169501a52197
samart1231 <samart88988@gmail.com>
2025-07-18 17:15:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -596,7 +596,6 @@ def delete_holiday(id):
 #     app.run(host='0.0.0.0', port=5000)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     init_db()
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=10000)
